2431093667a4755ee6f257455765509899aadc10
Emma Paris <yusei4520030401@gmail.com>
2022-08-26 05:25:11 +0900

    bootstrapをウェブから使うようにした Dockerとgoを入れた

--- js/display.js
+++ js/display.js
@@ -1,7 +1,7 @@
 // var num = "aaa";
 
-// document.getElementById('title1').innerHTML = num
-// lem.innerHTML = "<span style='color: red;'>span要素に変更したよ！</span>";
+//document.getElementById('title1').innerHTML = num
+//elem.innerHTML = "<span style='color: red;'>span要素に変更したよ！</span>";
 
-// var elem = document.getElementById("title2");
-// elem.innerHTML = "<span style='color: red;'>span要素に変更したよ！</span>";
+var elem = document.getElementById("title1");
+elem.innerHTML = "<span style='color: red;'>span要素に変更したよ！</span>";
