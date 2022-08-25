9892f67139241221264c6e19d58b95e9cbeb449e
OkamotoAi <okamotoai0217@icloud.com>
2022-08-25 14:48:20 +0900

    firebase

--- database.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import re
-
-df = pd.read_csv('StandardTablesofFood CompositionInJapan.csv')
-
-df['food_name'] = df['food_name'].replace(r"\（[^()]*\）", "")
-
-
-df.to_csv('calorie.csv', index=False)
