efbb06527fa827cbf6b49f2917e023e9a2d91fae
Emma Paris <yusei4520030401@gmail.com>
2022-08-25 19:23:39 +0900

    すべてのページの枠組みだけ作った

---
+++ js/display.js
@@ -0,0 +1,7 @@
+// var num = "aaa";
+
+// document.getElementById('title1').innerHTML = num
+// lem.innerHTML = "<span style='color: red;'>span要素に変更したよ！</span>";
+
+// var elem = document.getElementById("title2");
+// elem.innerHTML = "<span style='color: red;'>span要素に変更したよ！</span>";
---
+++ js/parse.js
@@ -0,0 +1,11 @@
+// const jsonData = require('../json/sample.json');
+
+// const json_string = JSON.stringify(jsonData);
+
+// const from_json = JSON.parse(json_string);
+
+// console.log(from_json[0].title) // 「JsonData1」が表示される
+
+// var num = "aaa";
+
+// document.getElementById('title1').innerHTML = num
