a33f03fd720737ee50640d8d2f4bcc5b4447a34d
OkamotoAi <okamotoai0217@icloud.com>
2022-08-27 01:42:46 +0900

    fix consume.json

--- python/calc_mets.py
+++ python/calc_mets.py
@@ -3,6 +3,7 @@ import json
 
 def main(weight,major,consume):
   with open('json/consume.json', 'w') as f:
+    f.write('[')
     df = pd.read_csv('python/mets.csv')
 
     # # 60kgの人
@@ -15,10 +16,15 @@ def main(weight,major,consume):
     df = df[df['major_heading']==major]
     df = df.sort_values('mets', ascending=False)
 
+    i=0
     for index, row in df.iterrows():
       minutes = int(60 * consume / (row['mets'] * weight * 1.05) )
       d = {'Activity':row['specific_activities'], 'minutes':minutes}
       json.dump(d, f, indent = 4, ensure_ascii=False)
+      if i < len(df)-1:
+          f.write(',')
+      i=i+1
+    f.write(']')
     f.close()
 
 if __name__ == "__main__":
